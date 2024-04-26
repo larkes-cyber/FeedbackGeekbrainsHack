36fb574d9cbeeca7a7f73f14d2c96ba096125a87
macbook <playegrovoy@gmail.com>
2024-04-27 02:50:42 +0300

    lections + course

--- src/components/list/LectionsList.js
+++ src/components/list/LectionsList.js
@@ -16,13 +16,13 @@ const LectionsList = (props) => {
 
     const [items, setItems] = useState([]);
 
-      
+   
 
     useEffect(() => {
         let newItems = [];
         props.items.forEach(element => {
             newItems.push(
-                <ExpendLection name={element.name} />
+                <ExpendLection course={element.course} lections={element.lections} lectionCallback={props.lectionCallback} />
             );
         });
         setItems(newItems);
@@ -57,15 +57,37 @@ const ExpendLection = (props) => {
                 <ListItemButton onClick={handleClick} sx={{ padding:1}} >
                     <ListItemText
                         primary = {
-                            props.name
-                    }
+                            props.course.name
+                        }
                     />
                     {open ? <ExpandLess /> : <ExpandMore />}
                 </ListItemButton>
             </Card>
           <Collapse in={open} timeout="auto" unmountOnExit>
               <List component="div" disablePadding>
-
+              {
+                props.lections.map((item) => {
+                    return(
+                            <Card
+                                elevation={2}
+                                sx={{mb:1, ml:1, mr:5, mt:1, width:"95%" }}
+                                >
+                                 <ListItemButton onClick={() => {
+                                    props.lectionCallback(item.id);
+                                }} sx={{ padding:1}} >
+                                    <ListItemText
+                                        primary = {
+                                        item.name
+                                        }
+                                        secondary = {
+                                            item.description
+                                        }
+                                    />
+                                </ListItemButton>
+                            </Card>
+                    )
+                })
+              }
               </List>
           </Collapse>
       </>
