ad70fc5767282cf1d6bd2877e3fac053ebd915a5
macbook <playegrovoy@gmail.com>
2024-04-27 03:04:14 +0300

    tabs

--- src/pages/tutor_page/TutorPage.js
+++ src/pages/tutor_page/TutorPage.js
@@ -147,30 +147,88 @@ const TutorPage = () => {
     return(
         <section className='tutor_page'>
             <div className='container'>
-                <div className='tutor_page__lection_wrapper'>
-                <Typography
-                        sx={{ display: 'inline', mt:"20px", ml:"1%"}}
-                        component="span"
-                        variant="h4"
-                        color="text.primary"
-                    >
-                Курсы/Лекции
-                </Typography>
-                <TextField
-                    id="outlined-size-small"
-                    placeholder='Поиск...'
-                    size="small"
-                    sx={{mt:"20px", mb:"15px", width:"95%", ml:"2%"}}
-                    onChange={(e) => {
-                        console.log(e.target.value)
-                    }}      
-                />
-                {
-                    courses != null ? <LectionsList items={courses} lectionCallback={(id) => {
-                        console.log(id);
-                    }} /> : null
+                <div className='tutor_page__wrapper'>
+                    <div className='tutor_page__lections'>
+                        <Typography
+                                sx={{ display: 'inline', mt:"40px", ml:"1%"}}
+                                component="span"
+                                variant="h4"
+                                color="text.primary"
+                            >
+                        Курсы/Лекции
+                        </Typography>
+                        <TextField
+                            id="outlined-size-small"
+                            placeholder='Поиск...'
+                            size="small"
+                            sx={{mt:"20px", mb:"15px", width:"95%", ml:"2%"}}
+                            onChange={(e) => {
+                                console.log(e.target.value)
+                            }}      
+                        />
+                        {
+                            courses != null ? <LectionsList items={courses} lectionCallback={(id) => {
+                                console.log(id);
+                            }} /> : null
 
-                }
+                        }
+                    </div>
+                    <div className='tutor_page__tabs'>
+                        <Box sx={{ width: '100%' }}>
+                                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                                <Tabs
+                                value={value} 
+                                onChange={handleChange} 
+                                aria-label="basic tabs example"
+                                textColor="secondary"
+                                indicatorColor="secondary"
+                                >
+                                <Tab label="Основное" />
+                                <Tab label="Комментарии"  />
+                                <Tab label="Графики"  />
+                                </Tabs>
+                                </Box>
+                                <CustomTabPanel value={value} index={0}>
+                                    <div className='big_block'>
+                                        
+                                    </div>
+                                </CustomTabPanel>
+                                <CustomTabPanel value={value} index={1}>
+                                    <CommentsList items = {[
+                                        {
+                                            title:"Дмитрий конь",
+                                            subtitle:"Да ебланы да посмотрите на них только. Вы клоуны как вы можете вообще такое преподовать, вот у нас в дтт вот там то да, препод на все 100. ее бы сюда к вам преподовать... "
+                                        },   {
+                                            title:"Дмитрий конь",
+                                            subtitle:"Да ебланы да посмотрите на них только. Вы клоуны как вы можете вообще такое преподовать, вот у нас в дтт вот там то да, препод на все 100. ее бы сюда к вам преподовать... "
+                                        },   {
+                                            title:"Дмитрий конь",
+                                            subtitle:"Да ебланы да посмотрите на них только. Вы клоуны как вы можете вообще такое преподовать, вот у нас в дтт вот там то да, препод на все 100. ее бы сюда к вам преподовать... "
+                                        },   {
+                                            title:"Дмитрий конь",
+                                            subtitle:"Да ебланы да посмотрите на них только. Вы клоуны как вы можете вообще такое преподовать, вот у нас в дтт вот там то да, препод на все 100. ее бы сюда к вам преподовать... "
+                                        },   {
+                                            title:"Дмитрий конь",
+                                            subtitle:"Да ебланы да посмотрите на них только. Вы клоуны как вы можете вообще такое преподовать, вот у нас в дтт вот там то да, препод на все 100. ее бы сюда к вам преподовать... "
+                                        },   {
+                                            title:"Дмитрий конь",
+                                            subtitle:"Да ебланы да посмотрите на них только. Вы клоуны как вы можете вообще такое преподовать, вот у нас в дтт вот там то да, препод на все 100. ее бы сюда к вам преподовать... "
+                                        },
+                                    ]} sx={{width:"60%"}}/>
+                                </CustomTabPanel>
+                                <CustomTabPanel value={value} index={2}>
+                                    <div className='student_page__tabs__chart'>
+                                    <ChartView/>
+                                    <ChartView/>
+                                    <ChartView/>
+                                    <ChartView/>
+                                    <ChartView/>
+                                    <ChartView/>
+
+                                    </div>
+                                </CustomTabPanel>
+                        </Box>
+                    </div>
                 </div>
             </div>
         </section>
